4d6e3834d7ba6fc9013323ed74a30c3b09f421e7
kirilk+1-at-701127717129 <kiril.kolessarsky@accenture.com>
2024-03-01 15:27:35 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 TODO:
-Create a simple flask-based TO-DO app. It should use a sqlite database to store the tasks and their status (blank || in progress || done)
+Create a simple flask-based TO-DO app. It should use a sqlite database to store the tasks and their status (unchecked || checked)
 The app should allow the user to create and delete tasks.
 After completion of the initial setup, the app should be containerized and the image pushed to dockerhub.
---
+++ todo_app/__init__.py
@@ -0,0 +1,70 @@
+from flask import Flask, render_template, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+from flask import request
+
+
+
+db = SQLAlchemy()
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
+    db.init_app(app)
+    
+    from .models import Task
+    with app.app_context():
+        db.create_all()
+        #sample_task = Task(title='Sample Task', status='TODO')
+    return app
+    
+
+
+
+app = create_app()
+
+
+def get_tasks():
+    from .models import Task
+    tasks = []
+    for i in range(len(Task.query.order_by(Task.id).all())):
+        tasks.append(Task.query.order_by(Task.id).all()[i])
+    
+    return tasks
+
+
+
+@app.route('/')
+def index():
+   
+    task_names = [i.title for i in get_tasks()]
+    status = [i.status for i in get_tasks()]
+    return render_template('index.html', tasks = get_tasks())
+
+
+@app.route('/add_task', methods=['POST'])
+def add_task():
+    from .models import Task
+    new_task = Task(title=request.form['newTask'])
+    db.session.add(new_task)
+    db.session.commit()  
+    return redirect(url_for('index'))
+
+@app.route('/delete_task', methods=['POST'])
+def delete_task():
+    from .models import Task
+    task = Task.query.filter_by(title=request.form['task_name']).first()
+    db.session.delete(task)
+    db.session.commit()
+    return redirect(url_for('index'))
+
+@app.route('/complete_task', methods=['POST'])
+def complete_task():
+    from .models import Task
+    task = Task.query.filter_by(title=request.form['task_name']).first()
+    if task.status == True:
+        task.status = False
+    else:
+        task.status = True
+    db.session.commit()
+    return redirect(url_for('index'))
+
